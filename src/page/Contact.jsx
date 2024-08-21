b27ad45bdc27b7362b1ea87304051ea497e301ef
RamiroUrt <121266524+RamiroUrt@users.noreply.github.com>
2024-08-21 20:49:30 -0300

    Update Contact.jsx

--- src/page/Contact.jsx
+++ src/page/Contact.jsx
@@ -1,5 +1,5 @@
 import Navbar from "../components/Navbar"
-import Footer from "../components/Footer"
+
 import Header from "../components/Header"
 import HeroContact from "../components/heroContact"
 
