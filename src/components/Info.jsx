0e5a0b76c2eedf2936ca0d07bb3f9fb2c8d92ace
Ramiro Urteaga <urteagaramiro33@gmail.com>
2024-08-23 10:23:57 -0300

    Update Info.jsx

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,4 +1,5 @@
 import { Parallax } from "react-parallax"
+import '../assets/css/Parallax.css'
 import Fondo from '../../public/img/info/fondo.jpg'
 import CardInfo from "./CardInfo"
 import Contact from "./Contact"
@@ -7,10 +8,10 @@ const Info = () => {
   return (
     <>
         <section className="info">
-            <Parallax strength={800} bgImage={Fondo}>
+            <Parallax strength={800} bgImage={Fondo} className="cover">
                     <div className="h-[auto] min-h-[80vh] mx-h-[100vh] flex justify-center items-center">
-                        <div className="card-info">
-                            <div className="cards gap-5 flex justify-center flex-wrap">
+                        <div className="card-info ">
+                            <div className="cards gap-5 flex justify-center flex-wrap ">
                                 <CardInfo tittle={'Horarios'} info={'De lunes a sabados \n 9am a 23pm'} />
                                 <CardInfo tittle={'Escribenos'} info={'3462656533'} />
                                 <CardInfo tittle={'Visitanos'} info={'Calle 123 \n Ciudad X'} />
